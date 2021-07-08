b3ec81c08d9eeb09f72636ef9f275f183bf07d6c
darkstar100x <darkstar100x@gmail.com>
2021-07-08 16:43:54 -0400

    Third Commit: More implementation code and updated gitignore as well

--- src/main/java/ucf/assignments/TodoListController.java
+++ src/main/java/ucf/assignments/TodoListController.java
@@ -1,5 +1,6 @@
 package ucf.assignments;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,6 +8,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+
+import java.util.Comparator;
+import java.util.function.Predicate;
 
 public class TodoListController {
   // All given FXML Fields:
@@ -49,12 +53,16 @@ public class TodoListController {
   public void addButtonClicked(ActionEvent actionEvent) {
     // Call addToList()
     // Call refresh()
+    addToList();
+    refresh();
   }
 
   @FXML
   public void removeButtonClicked(ActionEvent actionEvent) {
     // Call removeFromList() using selection from itemDisplay
     // Call refresh()
+    removeFromList(itemDisplay.getSelectionModel().getSelectedItem());
+    refresh();
   }
 
   @FXML
@@ -66,20 +74,39 @@ public class TodoListController {
   public void updateButtonClicked(ActionEvent actionEvent) {
     // Call updateItem() using selection from itemDisplay
     // Call refresh()
+    updateItem(itemDisplay.getSelectionModel().getSelectedItem());
+    refresh();
   }
 
   public void addToList() {
     // Bind completeBox to boolean value
     // Add new Item to items in itemList using the fields entered and boolean value
+    // Make sure sortedList updates as well
+    boolean flag = completeBox.isSelected();
+    Item i = new Item(descriptionField.getText(), dateField.getText(), flag);
+    itemList.addItem(i);
+    sortedList.add(i);
   }
 
   public void removeFromList(Item i) {
     // Remove i from "items" in itemList
+    // Make sure sortedList updates as well
+    itemList.removeItem(i);
+    sortedList.remove(i);
   }
 
   public void updateItem(Item i) {
     // Bind completeBox to boolean value
     // Edit i using the fields entered and boolean value
+    // Ensure the item is updated in both itemList and sortedList
+    itemList.removeItem(i);
+    sortedList.remove(i);
+    boolean flag = completeBox.isSelected();
+    i.setDescription(descriptionField.getText());
+    i.setDate(dateField.getText());
+    i.setCompletion(flag);
+    itemList.addItem(i);
+    sortedList.add(i);
   }
 
   @FXML
@@ -87,6 +114,9 @@ public class TodoListController {
     // Initialize itemList with placeholder title "List"
     // Set sortedList to "items" in itemList
     // Call refresh()
+    itemList = new ItemList("List");
+    sortedList = FXCollections.observableArrayList(itemList.getItems());
+    refresh();
   }
 
   @FXML
@@ -94,32 +124,42 @@ public class TodoListController {
     // Set description and date fields to "null"
     // Set completeBox to false
     // Set the items in itemDisplay equal to sortedList
+    descriptionField.setText(null);
+    dateField.setText(null);
+    completeBox.setSelected(false);
+    itemDisplay.setItems(sortedList);
   }
 
   @FXML
   public void dateSortClicked(ActionEvent actionEvent) {
     // Call dateSort()
     // Call refresh()
-    // Set items in display equal to sortedList
+    dateSort();
+    refresh();
   }
 
   public void dateSort() {
-    // If itemDisplay is already showing sortedList
-    // Set sortedList equal to the original list, un-sorting it
-    // Return
-    // Set sortedList equal to return value of itemList.sortByDate()
+    // Sort sortedList using date as a comparator
+    sortedList.sort(Comparator.comparing(Item::getDate));
   }
 
   @FXML
   public void showAllClicked(ActionEvent actionEvent) {
+    // Reset sortedList back to the full "items" in itemList
+    sortedList = FXCollections.observableArrayList(itemList.getItems());
+    refresh();
   }
 
   @FXML
   public void showCompleteClicked(ActionEvent actionEvent) {
+    sortedList.removeIf(Predicate.not(Item::isComplete));
+    refresh();
   }
 
   @FXML
   public void showIncompleteClicked(ActionEvent actionEvent) {
+    sortedList.removeIf(Item::isComplete);
+    refresh();
   }
 
   @FXML
