291e24ed5ae66f692efe8a71e591ffbc420446a1
darkstar100x <darkstar100x@gmail.com>
2021-07-11 13:37:18 -0400

    Sixth Commit: Implemented test code for Item.

--- src/test/java/ucf/assignments/ItemTest.java
+++ src/test/java/ucf/assignments/ItemTest.java
@@ -17,6 +17,10 @@ class ItemTest {
     // Assert the data is kept
     // Set description to a new String
     // Assert the new String is kept
+    Item testItem = new Item("TestDescription", "TestDate", false);
+    assertEquals("TestDescription", testItem.getDescription());
+    testItem.setDescription("NewDescription");
+    assertEquals("NewDescription", testItem.getDescription());
   }
 
   @Test
@@ -25,6 +29,10 @@ class ItemTest {
     // Assert the data is kept
     // Set date to a new String
     // Assert the new String is kept
+    Item testItem = new Item("TestDescription", "TestDate", false);
+    assertEquals("TestDate", testItem.getDate());
+    testItem.setDate("NewDate");
+    assertEquals("NewDate", testItem.getDate());
   }
 
   @Test
@@ -33,12 +41,29 @@ class ItemTest {
     // Assert the data is kept
     // Set completion to the opposite
     // Assert the new result
+    Item testItem = new Item("TestDescription", "TestDate", false);
+    assertFalse(testItem.isComplete());
+    testItem.setCompletion(true);
+    assertTrue(testItem.isComplete());
   }
 
   @Test
-  void testToString() {
-    // Instantiate Item using test data
+  void testToStringIncomplete() {
+    // Instantiate Item using test data (incomplete)
     // Create String for expected result
     // Assert the toString returns the same result
+    Item testItem = new Item("Test the program", "2021-07-10", false);
+    String expected = "On 2021-07-10: Test the program";
+    assertEquals(expected, testItem.toString());
+  }
+
+  @Test
+  void testToStringComplete() {
+    // Instantiate Item using test data (complete)
+    // Create String for expected result
+    // Assert the toString returns the same result
+    Item testItem = new Item("Test the program", "2021-07-10", true);
+    String expected = "On 2021-07-10: Test the program (COMPLETE)";
+    assertEquals(expected, testItem.toString());
   }
 }
