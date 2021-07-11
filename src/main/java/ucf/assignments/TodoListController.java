ebb9b8ee64560ca8482295e537fcd424f6fab351
darkstar100x <darkstar100x@gmail.com>
2021-07-11 17:36:44 -0400

    Eighth Commit: Implemented some more validator code.

--- src/main/java/ucf/assignments/TodoListController.java
+++ src/main/java/ucf/assignments/TodoListController.java
@@ -47,6 +47,25 @@ public class TodoListController {
   ObservableList<Item> filteredList;
 
   @FXML
+  public void initialize() {
+    // Initialize itemList with placeholder title "List"
+    // Set sortedList to "items" in itemList
+    // Call refresh()
+    itemList = new ItemList("List");
+    itemDisplay.setItems(itemList.getItems());
+    refresh();
+  }
+
+  @FXML
+  public void refresh() {
+    // Set description and date fields to "null"
+    // Set completeBox to false
+    descriptionField.setText(null);
+    dateField.setText(null);
+    completeBox.setSelected(false);
+  }
+
+  @FXML
   public void addButtonClicked(ActionEvent actionEvent) {
     // Call addToList()
     // Call refresh()
@@ -76,13 +95,72 @@ public class TodoListController {
     TodoListIO.saveList(itemList, pathField.getText());
   }
 
+  @FXML
+  public void loadButtonClicked(ActionEvent actionEvent) {
+    // Set itemList to a list loaded from the JSon file specified by pathField, using loadList in TodoListIO
+    // Set itemDisplay to itemList to update contents
+    // Call refresh()
+    itemList = TodoListIO.loadList(pathField.getText());
+    itemDisplay.setItems(itemList.getItems());
+    refresh();
+  }
+
+  @FXML
+  public void dateSortClicked(ActionEvent actionEvent) {
+    // Call dateSort() in itemList
+    // Call refresh()
+    itemList.dateSort();
+    refresh();
+  }
+
+  @FXML
+  public void showAllClicked(ActionEvent actionEvent) {
+    // Reset the display to the full list in itemList
+    itemDisplay.setItems(itemList.getItems());
+  }
+
+  @FXML
+  public void showCompleteClicked(ActionEvent actionEvent) {
+    // Call showComplete()
+    // Call refresh()
+    showComplete();
+    refresh();
+  }
+
+  @FXML
+  public void showIncompleteClicked(ActionEvent actionEvent) {
+    // Call showIncomplete()
+    // Call refresh()
+    showIncomplete();
+    refresh();
+  }
+
   public void addToList() {
     // Bind completeBox to boolean value
     // Add new Item to items in itemList using the fields entered and boolean value
     // Make sure sortedList updates as well
     boolean flag = completeBox.isSelected();
-    Item i = new Item(descriptionField.getText(), dateField.getText(), flag);
-    itemList.addItem(i);
+    boolean check = true;
+    if (InputValidator.isNull(descriptionField.getText())) {
+      descriptionField.setText("ERROR: No description entered to add");
+      check = false;
+    }
+    if (InputValidator.isNull(dateField.getText())) {
+      dateField.setText("ERROR: No date entered to add");
+      check = false;
+    }
+    if (!InputValidator.descriptionValidator(descriptionField.getText())) {
+      descriptionField.setText("ERROR: Invalid string entered for description");
+      check = false;
+    }
+    if (!InputValidator.dateValidator(dateField.getText())) {
+      dateField.setText("ERROR: Invalid string entered for date");
+      check = false;
+    }
+    if (check) {
+      Item i = new Item(descriptionField.getText(), dateField.getText(), flag);
+      itemList.addItem(i);
+    }
   }
 
   public void removeFromList(Item i) {
@@ -96,64 +174,34 @@ public class TodoListController {
     // Edit i using the fields entered and boolean value
     // Ensure the item is updated in both itemList and sortedList
     itemList.removeItem(i);
+    if (!InputValidator.isNull(descriptionField.getText())) {
+      if (InputValidator.descriptionValidator(descriptionField.getText())) {
+        i.setDescription(descriptionField.getText());
+      }
+      else {
+        descriptionField.setText("ERROR: Invalid description entered");
+      }
+    }
+    if (!InputValidator.isNull(dateField.getText())) {
+      if (InputValidator.dateValidator(dateField.getText())) {
+        i.setDate(dateField.getText());
+      }
+      else {
+        dateField.setText("ERROR: Invalid date entered");
+      }
+    }
     boolean flag = completeBox.isSelected();
-    if (descriptionField.getText() != null)
-      i.setDescription(descriptionField.getText());
-    if (dateField.getText() != null)
-      i.setDate(dateField.getText());
     i.setCompletion(flag);
     itemList.addItem(i);
   }
 
-  @FXML
-  public void initialize() {
-    // Initialize itemList with placeholder title "List"
-    // Set sortedList to "items" in itemList
-    // Call refresh()
-    itemList = new ItemList("List");
-    itemDisplay.setItems(itemList.getItems());
-    refresh();
-  }
-
-  @FXML
-  public void refresh() {
-    // Set description and date fields to "null"
-    // Set completeBox to false
-    // Set the items in itemDisplay equal to sortedList
-    descriptionField.setText(null);
-    dateField.setText(null);
-    completeBox.setSelected(false);
-  }
-
-  @FXML
-  public void dateSortClicked(ActionEvent actionEvent) {
-    // Call dateSort()
-    // Call refresh()
-    itemList.dateSort();
-    refresh();
-  }
-
-  @FXML
-  public void showAllClicked(ActionEvent actionEvent) {
-    itemDisplay.setItems(itemList.getItems());
-  }
-
-  @FXML
-  public void showCompleteClicked(ActionEvent actionEvent) {
+  public void showComplete() {
     filteredList = itemList.getItems().filtered(Item::isComplete);
     itemDisplay.setItems(filteredList);
   }
 
-  @FXML
-  public void showIncompleteClicked(ActionEvent actionEvent) {
+  public void showIncomplete() {
     filteredList = itemList.getItems().filtered(Predicate.not(Item::isComplete));
     itemDisplay.setItems(filteredList);
-  }
-
-  @FXML
-  public void loadButtonClicked(ActionEvent actionEvent) {
-    itemList = TodoListIO.loadList(pathField.getText());
-    itemDisplay.setItems(itemList.getItems());
-    refresh();
   }
 }
